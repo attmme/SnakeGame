2a355cf26d87cf86fa416e19f6cb78f6e9af8dea
cacauet <ogomezsts51l@gmail.com>
2020-07-27 17:12:59 +0200

    Add eating and size increase with movement

--- snake.py
+++ snake.py
@@ -1,19 +1,33 @@
 import turtle
 import time
 import snk_lib
+import random
 
 # Screen
 scr = turtle.Screen()
 snk_lib.screen()
 
-# Snake
+# Snake - add with function object
 snake = turtle.Turtle()
 snake.speed(0)
 snake.shape("square")
+snake.color("green")
 snake.penup()
 snake.goto(0, 0)
 snake.direction = "stop"
-snake.color("green")
+
+# Food - add with function object
+food = turtle.Turtle()
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(100,0)
+
+# Snake body
+snkBody = []
+
+
+## FUNCTIONS ##
 
 # Direction functions
 def mUp():
@@ -27,6 +41,7 @@ def mRight():
 
 def mLeft():
     snake.direction = "left"
+
 
 # Movement function
 def movement():
@@ -50,6 +65,46 @@ def movement():
         x = snake.xcor()
         snake.setx(x - 20)
 
+# Colision function
+def colision():
+
+    # Colision made with food
+    if snake.distance(food) < 20:
+        halfX = snk_lib.SCR_WIDTH/2
+        halfY = snk_lib.SCR_HEIGHT/2
+
+        # Food spawn inside the screen (with 40 of marge)
+        x = random.randint(-halfX+40, halfX-40)
+        y = random.randint(-halfY+40, halfY-40)
+        food.goto(x, y)
+
+        # Each time snake eats, its size will increase
+        newBody = turtle.Turtle()
+        newBody.speed(0)
+        newBody.shape("square")
+        newBody.color("grey")
+        newBody.penup()
+
+        snkBody.append(newBody)
+
+    # Snake body add and movement animation
+    totalBody = len(snkBody)
+
+    # Generate body from the end of the tail to the start
+    for i in range(totalBody-1, 0, -1):
+        x = snkBody[i-1].xcor()
+        y = snkBody[i-1].ycor()
+        
+        snkBody[i].goto(x, y)
+
+    # Set the first body part just after the 'head'
+    if totalBody > 0:
+        snkBody[0].goto(snake.xcor(), snake.ycor()) 
+        
+    # Colision made with wall or itself
+    # gameover
+
+
 # Keyboard
 scr.listen()
 scr.onkeypress(mUp, "Up")
@@ -59,6 +114,14 @@ scr.onkeypress(mLeft, "Left")
 
 # Main loop
 while True:
+    # Update screen
     scr.update()
+
+    # Check if there's a colision
+    colision()
+
+    # Keyboard movement detection
     movement()
+
+    # Delay to avoid overspeed
     time.sleep(snk_lib.SLEEP_T)
--- snk_lib.py
+++ snk_lib.py
@@ -1,11 +1,13 @@
 import turtle
 
 SLEEP_T = 0.1
+SCR_WIDTH = 600
+SCR_HEIGHT = 480
 
 # Screen
 def screen():
     scr = turtle.Screen()
     scr.title("Snake game")
     scr.bgcolor("black")
-    scr.setup(width=600, height=480)
+    scr.setup(width=SCR_WIDTH, height=SCR_HEIGHT)
     scr.tracer(0)
